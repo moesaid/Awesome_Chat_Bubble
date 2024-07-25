1d4b23d3b53b8e2eaa07c08abd365ce3c58f4bfa
moesaid <masedup@gmail.com>
2024-07-24 22:32:52 -0400

    init

---
+++ lib/src/awesome_chat_bubble.dart
@@ -0,0 +1,7 @@
+/// {@template awesome_chat_bubble}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class AwesomeChatBubble {
+  /// {@macro awesome_chat_bubble}
+  const AwesomeChatBubble();
+}
---
+++ test/src/awesome_chat_bubble_test.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:awesome_chat_bubble/awesome_chat_bubble.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('AwesomeChatBubble', () {
+    test('can be instantiated', () {
+      expect(AwesomeChatBubble(), isNotNull);
+    });
+  });
+}
